f72ea7a756ff4cacdd325dc892cae5b162bbeb39
Penny <sonaralee@users.noreply.github.com>
2018-06-02 22:23:21 -0500

    add web functionality to css project

--- src/css_fx/Controller.java
+++ src/css_fx/Controller.java
@@ -1,12 +1,76 @@
 package css_fx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Slider;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class Controller {
+
     @FXML
-    private Slider volume;
-    public void onButtonClicked() {
-        volume.adjustValue(50);
+    private Label label;
+    @FXML
+    private Button button4;
+    @FXML
+    private GridPane gridPane;
+    @FXML
+    private WebView webView;
+
+    public void initialize() {
+        button4.setEffect(new DropShadow());
     }
+
+    @FXML
+    public void handleMouseEnter() {
+        label.setScaleX(2.0);
+        label.setScaleY(2.0);
+    }
+
+    public void handleMouseExit() {
+        label.setScaleX(1.0);
+        label.setScaleY(1.0);
+    }
+
+    @FXML
+    public void handleClick() {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open Application File");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter(
+                        "Text", "*.txt"),
+                new FileChooser.ExtensionFilter(
+                        "PDF", "*.pdf"),
+                new FileChooser.ExtensionFilter(
+                        "All files", "*.*")
+        );
+
+
+        File file = chooser.showOpenDialog(gridPane.getScene().getWindow());
+        if(file != null) {
+            System.out.println(file.getPath());
+        } else {
+            System.out.println("Chooser was cancelled");
+        }
+    }
+
+    @FXML
+    public void handleLinkClick() {
+//        // load it in the OS default browser
+//        try {
+//            Desktop.getDesktop().browse(new URI("http://www.javafx.com"));
+//        } catch(IOException | URISyntaxException e) {
+//            e.printStackTrace();
+//        }
+
+        // load it in the WebView element
+        WebEngine engine = webView.getEngine();
+        engine.load("http://www.javafx.com");
+    }
+
 }
--- src/css_fx/Main.java
+++ src/css_fx/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass()
                                 .getResource("sample.fxml"));
-        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+        //setUserAgentStylesheet(STYLESHEET_CASPIAN);
 
         primaryStage.setTitle("Hello JavaFX!");
         primaryStage.setScene(new Scene(root, 600, 275));
